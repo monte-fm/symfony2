073961259a132448435eb5f0cd88d0f6152d9585
Olexander Kutsenko <monte-fm@users.noreply.github.com>
2016-03-16 17:09:15 +0200

    Update autostart.sh

--- configs/autostart.sh
+++ configs/autostart.sh
@@ -3,14 +3,3 @@ service php5-fpm start
 service nginx start
 service ssh start
 service mysql start
-
-shell /root/etckeeper.sh
-rm /root/etckeeper.sh
-
-echo "
-#!/bin/bash
-service php5-fpm start
-service nginx start
-service ssh start
-service mysql start
-" > /root/autostart.sh
